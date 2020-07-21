5ce1fea687ffb2dcf64155d0eb2515735a74d85c
mozzy11 <mozzymutesa@gmail.com>
2020-07-21 14:13:38 +0000

    add DayDrugDosage data model class and coresponding liquibase script

--- api/src/main/java/org/openmrs/module/ucionchology/models/DayDrugDosage.java
+++ api/src/main/java/org/openmrs/module/ucionchology/models/DayDrugDosage.java
@@ -1,6 +1,135 @@
 package org.openmrs.module.ucionchology.models;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-public class DayDrugDosage {
+import org.openmrs.BaseOpenmrsData;
+
+import javax.persistence.Entity;
+
+@Entity
+@Table(name = "drug_dosage")
+public class DayDrugDosage extends BaseOpenmrsData {
+
+	 
+    @Id
+	@GeneratedValue
+	@Column
+	private Integer id;
 	
+	
+	@Basic
+	@Column(length = 255, nullable = false)
+	private String doseName;
+	
+	@Basic
+	@Column(length = 255, nullable = false)
+	private String units;
+	
+	@Basic
+	@Column(nullable = false)
+	private int value;
+	
+	@Basic
+	@Column
+	private int age;
+	
+	@Basic
+	@Column
+	private int weight;  
+	
+	
+	@Basic
+	@Column
+	private int pltCount; 
+	
+	@Basic
+	@Column
+	private String hivStatus;
+	
+		
+	public String getDozeName() {
+		return doseName;
+	}
+
+	
+	public void setDozeName(String dozeName) {
+		this.doseName = dozeName;
+	}
+
+	
+	public String getUnits() {
+		return units;
+	}
+
+	
+	public void setUnits(String units) {
+		this.units = units;
+	}
+
+	
+	public int getValue() {
+		return value;
+	}
+
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	
+	public int getAge() {
+		return age;
+	}
+
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	
+	public int getWeight() {
+		return weight;
+	}
+
+	
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	
+	public int getPltCount() {
+		return pltCount;
+	}
+
+	
+	public void setPltCount(int pltCount) {
+		this.pltCount = pltCount;
+	}
+
+	
+	public String getHivStatus() {
+		return hivStatus;
+	}
+
+	
+	public void setHivStatus(String hivStatus) {
+		this.hivStatus = hivStatus;
+	}
+
+	@Override
+	public Integer getId() {
+		
+		return id;
+	}
+
+	@Override
+	public void setId(Integer id) {
+		this.id = id;
+		
+	}
+    
 }
